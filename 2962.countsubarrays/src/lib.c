9a55c5d92164796f96a2f73bfac7a1ab201590a5
Tony C Sidrock <tonysidrock@gmail.com>
2025-04-29 22:25:48 +0100

    next: next

--- 2962.countsubarrays/src/lib.c
+++ 2962.countsubarrays/src/lib.c
@@ -1,27 +1,41 @@
 #include <limits.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include "lib.h"
 
+void print_max_element_positions(int max_element_positions[]) {
+	for (int i = 0; max_element_positions[i] != -1; i++) {
+		printf("%d ", max_element_positions[i]);
+	}
+	printf("\n");
+}
+
 long long countSubarrays(int *nums, int numsSize, int k) {
 	int max_element = INT_MIN;
 	int max_element_positions[1024 * 1024] = {0};
-	for (int i = 0; i < numsSize; i++) {
-		if (max_element < nums[i])
+	for (int i = 0, p = 0; i < numsSize; i++) {
+		if (max_element < nums[i]) {
 			max_element = nums[i];
-		max_element_positions[i] = -1;
-	}
-	int max_element_count = 0;
-	long long array_count = 0;
-
-	for (int right = 0, rpos = 0; right < numsSize; right++) {
-		if (nums[right] == max_element) {
-			max_element_positions[rpos++] = right;
+			p = 0;
+		}
+		if (max_element == nums[i])
+			max_element_positions[p++] = i;
+		if (i == numsSize - 1) {
+			for (; p < numsSize; p++) {
+				max_element_positions[p] = -1;
+			}
 		}
 	}
 
-	if (max_element_count > k)
-		array_count++;
+	print_max_element_positions(max_element_positions);
+
+	long long array_count = 0;
+
+	for (int i = 0, j = k - 1; max_element_positions[j] > 0; i++, j++) {
+		array_count += (max_element_positions[i]) *
+			       (numsSize - max_element_positions[j]);
+	}
 
 	return array_count;
 }
