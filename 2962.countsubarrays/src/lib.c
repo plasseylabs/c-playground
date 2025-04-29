fcb7202dc0dadff0cddd8b43a76f975eca8d9457
Anton Sidorov <antosid@gmail.com>
2025-04-29 19:27:25 +0100

    next: notworking

--- 2962.countsubarrays/src/lib.c
+++ 2962.countsubarrays/src/lib.c
@@ -5,24 +5,23 @@
 
 long long countSubarrays(int *nums, int numsSize, int k) {
 	int max_element = INT_MIN;
+	int max_element_positions[1024 * 1024] = {0};
 	for (int i = 0; i < numsSize; i++) {
 		if (max_element < nums[i])
 			max_element = nums[i];
+		max_element_positions[i] = -1;
 	}
 	int max_element_count = 0;
 	long long array_count = 0;
 
-	for (int left = 0, right = 0; right < numsSize; right++) {
-		if (nums[right] == max_element)
-			++max_element_count;
-		if (max_element_count < k)
-			continue;
-		while (left <= right && max_element_count >= k) {
-			array_count += numsSize - right;
-			if (nums[left++] == max_element)
-				max_element_count--;
+	for (int right = 0, rpos = 0; right < numsSize; right++) {
+		if (nums[right] == max_element) {
+			max_element_positions[rpos++] = right;
 		}
 	}
+
+	if (max_element_count > k)
+		array_count++;
 
 	return array_count;
 }
