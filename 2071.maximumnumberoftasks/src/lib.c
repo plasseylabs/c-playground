1189e7981252779454f5875adf7728a9bf558d5b
Anton Sidorov <antosid@gmail.com>
2025-05-01 18:20:38 +0100

    next: 65ms

--- 2071.maximumnumberoftasks/src/lib.c
+++ 2071.maximumnumberoftasks/src/lib.c
@@ -14,61 +14,51 @@ void array_display(int a[], int a_length) {
 
 int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
 
-void sort_ints(int *array, size_t length) {
-	qsort(array, length, sizeof(int), compare);
+int check(int *tasks, int *workers, int workersSize, int pills, int strength,
+	  int median) {
+	int ws[workersSize];
+	int ptr = workersSize - 1;
+	int head = workersSize - 1, tail = workersSize - 1;
+	for (int i = median - 1; i >= 0; --i) {
+		while (ptr >= workersSize - median &&
+		       workers[ptr] + strength >= tasks[i]) {
+			ws[head] = workers[ptr];
+			--head;
+			--ptr;
+		}
+		if (head == tail) {
+			return 0;
+		} else if (ws[tail] >= tasks[i]) {
+			tail--;
+		} else {
+			if (!pills) {
+				return 0;
+			}
+			--pills;
+			head++;
+		}
+	}
+
+	return 1;
 }
 
 int maxTaskAssign(int *tasks, int tasksSize, int *workers, int workersSize,
 		  int pills, int strength) {
-	int max_task_number = 0;
+	qsort(tasks, tasksSize, sizeof(int), compare);
+	qsort(workers, workersSize, sizeof(int), compare);
 
-	sort_ints(tasks, tasksSize);
-	sort_ints(workers, workersSize);
-
-	array_display(tasks, tasksSize);
-	array_display(workers, workersSize);
-
-	for (int i = 0, j = 0; i < tasksSize && j < workersSize; i++) {
-		int taken = 0;
-		for (int k = j; k < workersSize; k++) {
-			/*
-					  printf("count: %3d, task: %3d - %5d,
-			   worker: "
-						 "%5d - %5d, "
-						 "\n",
-						 max_task_number, i, tasks[i],
-			   k, workers[k]); printf("count: %3d, task: %3d - %5d,
-			   worker: "
-						 "%5d - %5d, "
-						 "pills: %3d\n",
-						 max_task_number, i, tasks[i],
-			   k, workers[k] + strength, pills);
-			*/
-			if (workers[k] >= tasks[i]) {
-				taken++;
-				max_task_number++;
-				j++;
-				break;
-			}
-			assert(taken || (workers[k] < tasks[i]));
-		}
-		if (taken == 1) {
-			continue;
-		}
-
-		for (int k = j; k < workersSize; k++) {
-			if (taken == 0 && pills > 0 &&
-			    ((workers[k] + strength) >= tasks[i])) {
-				taken++;
-				pills--;
-				max_task_number++;
-				j++;
-				break;
-			}
-			assert((workers[k] + strength * (pills > 0) ? 1 : 0) <
-			       tasks[i]);
+	int left = 1,
+	    right = (tasksSize < workersSize) ? tasksSize : workersSize,
+	    max_number = 0;
+	while (left <= right) {
+		int median = (left + right) / 2;
+		if (check(tasks, workers, workersSize, pills, strength,
+			  median)) {
+			max_number = median;
+			left = median + 1;
+		} else {
+			right = median - 1;
 		}
 	}
-
-	return max_task_number;
+	return max_number;
 }
