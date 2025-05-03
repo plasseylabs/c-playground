2401f683e5cc2d8b4f18779bb04fb237cf2353ae
Tony C Sidrock <tonysidrock@gmail.com>
2025-05-03 17:16:44 +0100

    minimalistic

--- 5.longestpalindromic/src/lib.c
+++ 5.longestpalindromic/src/lib.c
@@ -3,34 +3,28 @@
 
 #include "lib.h"
 
+int check_palindrome(const char *s, int l, int r) {
+	for (; l < r;) {
+		if (s[l++] != s[r--]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
 char *longestPalindrome(char *s) {
 	if (!s)
 		return NULL;
 	int string_length = strlen(s);
-	int start = 0, candidate = 0, longest_length = 0;
+	int start = 0, longest_length = 0;
 
 	for (int left = 0; left < string_length - longest_length; ++left) {
 		for (int right = string_length - 1;
 		     right - left > longest_length; --right) {
-			if (s[left] != s[right])
-				continue;
-			int current_length = right - left;
-			if (current_length <= longest_length) {
-				candidate = 0;
-				break;
-			}
-			int l = left + 1, r = right - 1;
-			candidate = 1;
-			for (; l < r; ++l, --r) {
-				if (s[l] != s[r]) {
-					candidate = 0;
-					break;
-				}
-			}
-			if (!candidate)
+			if (!check_palindrome(s, left, right))
 				continue;
 			start = left;
-			longest_length = current_length;
+			longest_length = right - left;
 		}
 	}
 
