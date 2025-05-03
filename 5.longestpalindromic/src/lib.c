b8390cacfa0dbdb52af6d6515382267252519a66
Tony C Sidrock <tonysidrock@gmail.com>
2025-05-03 14:29:46 +0100

    93ms45

--- 5.longestpalindromic/src/lib.c
+++ 5.longestpalindromic/src/lib.c
@@ -12,21 +12,23 @@ char *longestPalindrome(char *s) {
 	int string_length = strlen(s);
 	char longest[MAX_LENGTH] = {0};
 	longest[0] = s[0];
-	int longest_length = 1;
+	int longest_length = 0;
 
 	for (int left = 0; left < string_length - longest_length; ++left) {
-		for (int right = string_length - 1; right > left; --right) {
+		for (int right = string_length - 1;
+		     right - left > longest_length; --right) {
 			if (s[left] != s[right])
 				continue;
 			int l = left + 1, r = right - 1, candidate = 1;
 			for (; l < r; ++l, --r) {
-				if (s[l] != s[r])
+				if (s[l] != s[r]) {
 					candidate = 0;
-				break;
+					break;
+				}
 			}
 			if (!candidate || r - l > 1)
 				continue;
-			int current_length = right - left + 1;
+			int current_length = right - left;
 			if (current_length > longest_length) {
 				for (int k = 0, i = left; i <= right;
 				     ++k, ++i) {
@@ -36,8 +38,8 @@ char *longestPalindrome(char *s) {
 			}
 		}
 	}
-	char *result = (char *)calloc(longest_length, sizeof(char *));
-	memcpy(result, longest, longest_length);
+	char *result = (char *)calloc(longest_length + 1, sizeof(char *));
+	memcpy(result, longest, longest_length + 1);
 
 	return result;
 }
