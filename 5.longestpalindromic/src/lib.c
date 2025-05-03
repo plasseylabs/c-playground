4a4e295d7ae4ac71183e56fb92ebe06526173a47
Tony C Sidrock <tonysidrock@gmail.com>
2025-05-03 16:28:24 +0100

    72ms

--- 5.longestpalindromic/src/lib.c
+++ 5.longestpalindromic/src/lib.c
@@ -1,45 +1,39 @@
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "lib.h"
 
-#define MAX_LENGTH 1024
-
 char *longestPalindrome(char *s) {
 	if (!s)
 		return NULL;
 	int string_length = strlen(s);
-	char longest[MAX_LENGTH] = {0};
-	longest[0] = s[0];
-	int longest_length = 0;
+	int start = 0, candidate = 0, longest_length = 0;
 
 	for (int left = 0; left < string_length - longest_length; ++left) {
 		for (int right = string_length - 1;
 		     right - left > longest_length; --right) {
 			if (s[left] != s[right])
 				continue;
-			int l = left + 1, r = right - 1, candidate = 1;
+			int current_length = right - left;
+			if (current_length <= longest_length) {
+				candidate = 0;
+				break;
+			}
+			int l = left + 1, r = right - 1;
+			candidate = 1;
 			for (; l < r; ++l, --r) {
 				if (s[l] != s[r]) {
 					candidate = 0;
 					break;
 				}
 			}
-			if (!candidate || r - l > 1)
+			if (!candidate)
 				continue;
-			int current_length = right - left;
-			if (current_length > longest_length) {
-				for (int k = 0, i = left; i <= right;
-				     ++k, ++i) {
-					longest[k] = s[i];
-				}
-				longest_length = current_length;
-			}
+			start = left;
+			longest_length = current_length;
 		}
 	}
-	char *result = (char *)calloc(longest_length + 1, sizeof(char *));
-	memcpy(result, longest, longest_length + 1);
 
-	return result;
+	s[start + longest_length + 1] = '\0';
+	return s + start;
 }
--- 5.longestpalindromic/test/tests.c
+++ 5.longestpalindromic/test/tests.c
@@ -8,45 +8,38 @@
 int main(void) {
 	char *result = NULL;
 
-	char *line1 = "babad";
+	char line1[] = "babad";
 	result = longestPalindrome(line1);
 	printf("%s longest substing: %s\n", line1, result);
 	assert(!strcmp(result, "bab"));
-	if (result)
-		free(result);
 
-	char *line2 = "cbbd";
+	char line2[] = "cbbd";
 	result = longestPalindrome(line2);
 	printf("%s longest substing: %s\n", line2, result);
 	assert(!strcmp(result, "bb"));
-	if (result)
-		free(result);
 
-	char *line3 = "pwwkew";
+	char line3[] = "pwwkew";
 	result = longestPalindrome(line3);
 	printf("%s longest substing: %s\n", line3, result);
 	assert(!strcmp(result, "ww"));
-	if (result)
-		free(result);
 
-	char *line4 = "au";
+	char line4[] = "au";
 	result = longestPalindrome(line4);
 	printf("%s longest substing: %s\n", line4, result);
 	assert(!strcmp(result, "a"));
-	if (result)
-		free(result);
 
-	char *line5 = "aab";
+	char line5[] = "aab";
 	result = longestPalindrome(line5);
 	printf("%s longest substing: %s\n", line5, result);
 	assert(!strcmp(result, "aa"));
-	if (result)
-		free(result);
 
-	char *line6 = "aaaaa";
+	char line6[] = "aaaaa";
 	result = longestPalindrome(line6);
 	printf("%s longest substing: %s\n", line6, result);
 	assert(!strcmp(result, "aaaaa"));
-	if (result)
-		free(result);
+
+	char line7[] = "aaaabaaa";
+	result = longestPalindrome(line7);
+	printf("%s longest substing: %s\n", line7, result);
+	assert(!strcmp(result, "aaabaaa"));
 }
